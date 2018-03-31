3a9c9afe4a98cc65dd5bd7e1c03fba9e49a26413
Sd-Imtiyaz <34977291+Sd-Imtiyaz@users.noreply.github.com>
2018-03-31 14:48:15 +0530

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,25 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(0)
+
+def circle(length,angle):
+    for i in range(4):
+        my_turtle.forward(length)
+        my_turtle.right(angle)
+
+   
+   
+    
+
+
+#circle(length,angle)
+for i in range(100):
+    circle(100,90)
+    my_turtle.right(11)
+
+#circle has 360 Degrees
+    #360/10--->36
+
+    
+    
